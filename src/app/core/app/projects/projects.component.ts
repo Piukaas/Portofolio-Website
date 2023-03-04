bb0ba1894f0c6b7021d0fb4e4ef6c7ab40002602
Thijs <thijsvanrijsbergen@gmail.com>
2023-03-04 23:57:07 +0100

    all projects

--- src/app/core/app/projects/projects.component.ts
+++ src/app/core/app/projects/projects.component.ts
@@ -4,4 +4,25 @@ import { Component } from "@angular/core";
   selector: "app-projects",
   templateUrl: "./projects.component.html",
 })
-export class ProjectsComponent {}
+export class ProjectsComponent {
+  is2023 = false;
+  is2022 = false;
+  is2021 = false;
+  is2020 = false;
+
+  toggle2023() {
+    this.is2023 = !this.is2023;
+  }
+
+  toggle2022() {
+    this.is2022 = !this.is2022;
+  }
+
+  toggle2021() {
+    this.is2021 = !this.is2021;
+  }
+
+  toggle2020() {
+    this.is2020 = !this.is2020;
+  }
+}
