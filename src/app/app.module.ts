cfd5a104f72ae31de28cd5280c8f961c6d03f6cf
Thijs <thijsvanrijsbergen@gmail.com>
2023-03-04 18:32:44 +0100

    some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { CommonModule } from '@angular/common'
-import { HttpClientModule } from '@angular/common/http'
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { BrowserModule } from '@angular/platform-browser'
-import { RouterModule } from '@angular/router'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { LocalStorageModule } from 'angular-2-local-storage-encrypt'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './core/app/app.component'
-import { DashboardComponent } from './core/app/dashboard/dashboard.component'
-import { FooterComponent } from './core/app/footer/footer.component'
-import { ProjectsComponent } from './core/app/projects/projects.component'
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { LocalStorageModule } from "angular-2-local-storage-encrypt";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./core/app/app.component";
+import { DashboardComponent } from "./core/app/dashboard/dashboard.component";
+import { ProjectsComponent } from "./core/app/projects/projects.component";
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, ProjectsComponent, FooterComponent],
+  declarations: [AppComponent, DashboardComponent, ProjectsComponent],
   imports: [
     CommonModule,
     BrowserModule,
@@ -24,18 +23,18 @@ import { ProjectsComponent } from './core/app/projects/projects.component'
     ReactiveFormsModule,
     HttpClientModule,
     LocalStorageModule.forRoot({
-      prefix: '',
-      storageType: 'localStorage',
+      prefix: "",
+      storageType: "localStorage",
       encryptionActive: true,
       encryptionOptions: {
-        encryptionKey: 'secret',
-        encryptionIv: '123',
-        encryptionSalt: 'salty'
-      }
-    })
+        encryptionKey: "secret",
+        encryptionIv: "123",
+        encryptionSalt: "salty",
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule {}
--- src/app/core/app/projects/projects.component.ts
+++ src/app/core/app/projects/projects.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core'
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html'
 })
 export class ProjectsComponent {
-  constructor(private modalService: NgbModal) {}
-
-  // Open modal (Used for image preview)
-  open(content: any) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result
-  }
 }
