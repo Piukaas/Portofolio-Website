3828fa4139e5881ef0e54f4e91f0551106b1837f
Thijs <thijsvanrijsbergen@gmail.com>
2023-03-05 02:29:02 +0100

    big modal

--- src/app/core/app/projects/projects.component.ts
+++ src/app/core/app/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
+import { ModalService } from "../_services/modal.service";
 
 @Component({
   selector: "app-projects",
   templateUrl: "./projects.component.html",
 })
 export class ProjectsComponent {
+  @ViewChild("portofolio") portofolio: ElementRef | undefined;
+
   is2023 = false;
   is2022 = false;
   is2021 = false;
   is2020 = false;
+
+  constructor(private modalService: ModalService) {}
 
   toggle2023() {
     this.is2023 = !this.is2023;
@@ -24,5 +29,9 @@ export class ProjectsComponent {
 
   toggle2020() {
     this.is2020 = !this.is2020;
+  }
+
+  openModal(el: any) {
+    this.modalService.open(el);
   }
 }
